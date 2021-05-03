006b03407b18395d0b775bc2f3bf2bbc5ca64f5f
Maxwell Sands <maxsands1503@icloud.com>
2021-05-03 17:54:21 -0600

    added some routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './App.css';
 import {Homepage} from "./pages/hompage.component";
+import  { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => {
+    return (
+        <h1>Hats Page</h1>
+    )
+}
+
+const HatsDetailPage = props => {
+    return (
+        <h1>Hats Page {props.match.params.hatId}</h1>
+    )
+}
+
+const PageNotFound = () => {
+    return (
+        <h1>This page could not be found!</h1>
+    )
+}
+
 function App() {
   return (
     <div>
-      <Homepage></Homepage>
+        <Switch>
+            <Route exact path='/' component={Homepage}/>
+            <Route exact path='/hats' component={HatsPage}/>
+            <Route path='/hats/:hatId' component={HatsDetailPage}/>
+            <Route path='*'>
+                <PageNotFound />
+            </Route>
+        </Switch>
+
+
+
     </div>
   );
 }
