b3a06c4daa766f606b14a2f72a902e0a5d72e0e1
Maxwell Sands <maxsands1503@icloud.com>
2021-05-05 05:33:04 -0600

    added a header component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {Homepage} from "./pages/hompage.component";
 import  { Route, Switch } from "react-router-dom";
 import Shop from "./pages/shop/shop";
-
+import Header from "./components/header/header";
 const PageNotFound = () => {
     return (
         <h1>This page could not be found!</h1>
@@ -13,6 +13,7 @@ const PageNotFound = () => {
 function App() {
   return (
     <div>
+        <Header />
         <Switch>
             <Route exact path='/' component={Homepage}/>
             <Route exact path='/shop' component={Shop} />
